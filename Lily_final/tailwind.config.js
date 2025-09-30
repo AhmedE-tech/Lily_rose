b74b117610576a963e37ea07ad45672f7108c8aa
AhmedE-tech <ahmedessam@outlook.de>
2025-09-30 21:03:34 +0300

    Add files via upload

---
+++ Lily_final/tailwind.config.js
@@ -0,0 +1,16 @@
+// Lily/frontend/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'pulse-slow': 'pulse 3s linear infinite',
+      }
+    },
+  },
+  plugins: [],
+}
